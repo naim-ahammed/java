732c5ac7d266e851af02ce20f54e9145e295b741
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2023-03-23 10:31:19 +0600

    added

---
+++ FirstClass.java
@@ -0,0 +1,296 @@
+
+import java.util.*;
+
+class FristClass {
+
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+
+
+    /*  //function2
+    public static void printFactorial(int n){
+
+        if(n<0){
+            System.out.println("invalid number");
+            return;
+        }
+        int factorial = 1;
+
+        for(int i=n; i>=1; i--){
+            factorial = factorial*i;
+        }
+        System.out.println(factorial);
+        return;
+    }*/
+
+/*public static int sumNumber1(int a, int b){
+int sum = a+b;
+return sum;
+} */
+
+public static void main(String args[]){
+
+
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+    /* */
+
+    /*     //Factorial2
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    printFactorial(n);*/
+    
+    /*     Scanner sm = new Scanner(System.in);
+    System.out.print("Enter a = ");
+    int a = sm.nextInt();
+    System.out.print("Enter b = ");
+    int b = sm.nextInt();
+
+    int sum = sumNumber1(a,b);
+    System.out.print("the sum is = "+ sum);*/
+
+
+
+    /*    int n = 5;
+
+    for(int i=1; i<=n; i++){
+        //spaces
+        for(int j=1; j<=n-1; j++){
+            System.out.print("  ");
+        }
+        //numbers->print row no,row no times
+        for(int j=1; j<=i; j++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    } */
+
+
+
+
+     Scanner nu = new Scanner(System.in);
+    System.out.println("Enter how many number you see = ");
+    int num = nu.nextInt();
+
+    int i ;
+
+    for(i=0; i<num; i++){
+        System.out.println( i + "." + " hello world");
+    }
+
+
+
+
+    /*    
+    int i=0;
+    do{
+        System.out.println(c);
+        i++;
+    }while(i<10); */
+
+
+
+
+
+    /*     
+    int i=0;
+    while(i<10){
+        System.out.println( i + "." + " hello world");
+        i++;
+    }*/
+
+
+
+
+    /*    
+    for(int count = 0; count<=10; count++){
+    System.out.println(count+"."+" hello world");
+    } */
+
+
+
+
+    /*     
+    Scanner sc = new Scanner(System.in);
+    System.out.println("press the button");
+
+    int button = sc.nextInt();
+
+    switch(button){
+        case 1:
+        System.out.println("equal");
+        break;
+
+        case 2:
+        System.out.println("two");
+        break;
+
+        case 3:
+        System.out.println("three");
+        break;
+
+        case 4:
+        System.out.println("four");
+        break;
+    }*/
+
+
+
+    
+    /*
+         Scanner sc = new Scanner(System.in);
+    System.out.println("press the button");
+
+    int button = sc.nextInt();
+
+    switch(button){
+        case 1:
+        System.out.println("equal");
+        break;
+
+        case 2:
+        System.out.println("two");
+        break;
+
+        case 3:
+        System.out.println("three");
+        break;
+
+        case 4:
+        System.out.println("four");
+        break;
+    }*/
+
+
+
+
+    /*
+        Scanner sc = new Scanner(System.in);
+    System.out.println("press the button");
+
+    int button = sc.nextInt();
+
+    if(button == 1){
+        System.out.println("equal");
+    }
+    else if(button == 2){
+        System.out.println("grater");
+    }
+    else if(button == 3){
+        System.out.println("lasser");
+    }
+    else{
+        System.out.println("not mach");
+    }*/
+
+
+
+
+    /*
+         Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number A , B ");
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    if(a==b){
+        System.out.println("equal");
+    }
+    else if(a>b){
+        System.out.println("A is grater");
+    }
+    else if(a<b){
+        System.out.println("A is lasser");
+    }
+    else{
+        System.out.println("not mach");
+    }*/
+
+
+
+    /* 
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    if(x % 2 == 0 ){
+        System.out.println("even");
+    }
+    else{
+        System.out.println("odd");
+    }*/
+
+
+
+    /*     Scanner sc = new Scanner(System.in);
+    int age = sc.nextInt();
+    if(age>18){
+        System.out.println("adult");
+    }
+    else{
+        System.out.println("not adult");
+    }*/
+
+
+
+    /*
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a+b;
+    System.out.println(sum);*/
+
+
+
+
+    /*
+    //import
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    //nextInt()
+    //nextFloat()
+    System.out.println(name);*/
+
+
+    
+    /*
+    int a,b,sum,diff,mul,div,ans;
+    a=10;
+    b=20;
+    sum=a+b;
+    diff=b-a;
+    mul=b*a;
+    div=b/a;
+    ans=(b*a)/(b-a);
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(mul);
+    System.out.println(div);
+    System.out.println(ans);*/
+
+    
+    /*
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+    System.out.println("*\n**");
+    System.out.println("naim");
+    System.out.println("naim");
+    System.out.print("naim\n");
+    System.out.print("naim\nahammed\nlabib");*/    
+
+    
+}
+}
